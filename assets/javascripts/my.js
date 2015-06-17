9b659bb4668f3c17d156df1fd8baaf17a99fe1c0
cromozooom <cromozooom@gmail.com>
2015-06-17 11:28:47 +0200

    logo and services update

--- assets/javascripts/my.js
+++ assets/javascripts/my.js
@@ -1,3 +1,13 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).ready(function () {
+	$('svg symbol').popover({
+		'trigger':'hover',
+		'container': 'body',
+		'placement': 'bottom',
+		'white-space': 'nowrap',
+		'html':'true'
+	});
+});
